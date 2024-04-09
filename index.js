eddf1fcf6130ff63114a62ac33f86188588cdf35
dilbot-cot <131760791+dilbot-cot@users.noreply.github.com>
2024-04-09 19:50:39 +1000

    feat!: installed pokemon package

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log("Terminal app is running");
+
+console.log("Bye bye, terminal app finished");
+
+const pokemon = require('pokemon');
+
+pokemonId = pokemon.getId('Tauros')
+console.log(`Tauros's pokedex ID is ${pokemonId}`);
